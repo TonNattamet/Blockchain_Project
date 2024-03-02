499877be05bbb7ec26b8d46d8bc6779d923f4edf
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-03-03 00:52:25 +0700

    Create Navbar

---
+++ Blockchain_BTMK/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+        <h1>Test</h1>
+        <div className="Navbar">
+            <h1>BTMK</h1>
+            <div className="item">
+                <h3>Home</h3>
+                <h3>About</h3>
+                <h3>Service</h3>
+                <h3>Blog</h3>
+                <h3>Contact</h3>
+                <h3>Search</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
