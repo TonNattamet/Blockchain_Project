48fe2b687fee0e433cf024e5828dffd1ec4160b4
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-03-05 03:13:03 +0700

    editing

--- Blockchain_BTMK/src/component/Home/Home.jsx
+++ Blockchain_BTMK/src/component/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Home.css'
+import Imgage from '../Image/bg-home.png'
 import Navbar from '../Navbar/Navbar'
 
 function Home() {
   return (
     <div>
+      
         <Navbar/>
         <div className="main">
+        <img src="../Image/bg-home.png" alt="" />
             <h1>Hello Blockchain Project</h1>
         </div>
     </div>
