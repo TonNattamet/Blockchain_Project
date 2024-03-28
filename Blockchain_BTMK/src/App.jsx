1614b6d644037f1a75c1d8feef7cdf80e2cb4dd6
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-03-29 01:57:12 +0700

    Addinformation

--- Blockchain_BTMK/src/App.jsx
+++ Blockchain_BTMK/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './component/Home/Home';
 import Search from './component/Search/Search';
 import Login from './component/Login/Login';
 import Information from './component/Informtion/Information';
+import AddInfo from './component/AddInfo/AddInfo';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
                 <Route path='/search' element={<Search />}></Route>
                 <Route path='/login' element={<Login />}></Route>
                 <Route path='/information' element={<Information />}></Route>
+                <Route path='/addInfo' element={<AddInfo />}></Route>
             </Routes>
         </BrowserRouter>
     </>
--- Blockchain_BTMK/src/component/AddInfo/AddInfo.jsx
+++ Blockchain_BTMK/src/component/AddInfo/AddInfo.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import './Addinfo.css'
 
 function AddInfo() {
   return (
     <div>
-        
+      <Navbar/>
+        <h1>AddInformation</h1>
+            <div className="main-info">
+                <div className="box-id">
+                    ID
+                    <input type="text" />
+                </div>
+                <div className="box-name">
+                    Full Name
+                    <input type="text" />
+                </div>
+                <div className="box-phone">
+                    Phone
+                    <input type="text" />
+                </div>
+                <div className="box-gender">
+                    <legend>Gender</legend>
+                    <label htmlFor="male">Male</label>
+                    <input type="radio" id='male' value="male" />
+
+                    <label htmlFor="female">Female</label>
+                    <input className="female" type="radio" id='female' value="female" />
+                </div>
+                
+                <div className="box-date">
+                    Date of Birth
+                    <input type="text" />
+                </div>
+                <div className="box-height-weight">
+                    <div className="box-height">
+                    Height (CM)
+                        <input type="text" />
+                    </div>
+                    <div className="box-weight">
+                    Weight (KG)
+                        <input type="text" />
+                    </div>
+                </div>
+                <div className="box-address">
+                    Address
+                    <input type="text" />
+                </div>
+        </div>
     </div>
   )
 }
