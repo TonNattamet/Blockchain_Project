f742316fbab19471b2ad82f947057be515cb368c
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-03-30 00:51:47 +0700

    add head main list

--- Blockchain_BTMK/src/component/ListUser/ListUser.jsx
+++ Blockchain_BTMK/src/component/ListUser/ListUser.jsx
@@ -15,6 +15,50 @@ function ListUser() {
                 <input type="text" placeholder='Search'/>
             </div>
         </div>
+
+        <div className="box-main-list">
+            <div className="head-main-list">
+                <div className="search-id">
+                    ID
+                </div>
+                <div className="search-name">
+                    Full Name
+                </div>
+                <div className="search-birth">
+                    Brithday
+                </div>
+                <div className="search-age">
+                    Age
+                </div>
+                <div className="search-gender">
+                    Gender
+                </div>
+                <div className="search-action">
+                    Action
+                </div>
+            </div>
+            <div className="main-list">
+                <div className="id-list">
+                    1234567890123
+                </div>
+                <div className="name-list">
+                    Tanawat Kama
+                </div>
+                <div className="birth-list">
+                    01/01/1111
+                </div>
+                <div className="age-list">
+                    21
+                </div>
+                <div className="gender-list">
+                    Male
+                </div>
+                <div className="action-list">
+                    Logo list
+                </div>
+                
+            </div>
+        </div>
     </div>
   )
 }
