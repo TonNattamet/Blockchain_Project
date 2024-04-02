5ddd6efc6d8a1a1ba82506554288e6ad4a877b10
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-02 16:15:57 +0700

    update login function

--- Blockchain_BTMK/src/component/Login/Login.jsx
+++ Blockchain_BTMK/src/component/Login/Login.jsx
@@ -2,24 +2,39 @@ import React, {useState}  from 'react'
 import './Login.css'
 import Navbar from '../Navbar/Navbar'
 import {Link} from 'react-router-dom'
+import axios, { Axios } from 'axios'
 
 function Login() {
 
-  return (
+    const [id, setId] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(event){
+        event.prevenDefault();
+        axios.post('http://localhost:8081/login', {id, password})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+
+    return (
     <div>
         <Navbar/>
         <div className="main-login">
             <h3>Login Patient</h3>
             <p>welcome to website</p>
-            <input type="text" placeholder='ID' required/>
-            <input type="text" placeholder='Password' required/>
+            <input type="text" placeholder='ID' 
+            onChange={e => setId(e.target.value)}
+            required/>
+            <input type="text" placeholder='Password' 
+            onChange={e => setPassword(e.target.value)}
+            required/>
             <div className="box-select-forgot">
                 <Link to='/register'>Register</Link>
                 <Link to='/logindoctor'>Login Doctor</Link>
             </div>
 
             <div className="box-login">
-                <Link to='/home'>Login</Link>
+                <Link onSubmit={handleSubmit} >Login</Link>
             </div>
         </div>
         
