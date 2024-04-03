0f365e2fb18227ddaa5e6c29da299ffd30368db2
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-03 21:24:38 +0700

    Update and Test New Table

--- Blockchain_BTMK/src/component/Search/Search.jsx
+++ Blockchain_BTMK/src/component/Search/Search.jsx
@@ -6,11 +6,57 @@ function Search() {
   return (
     <div className='main-search'>
         <Navbar/>
-        <div className="main-box-search">
-            <h3>ID Patient</h3>
-            <div className="box-search">
+        <div className="box-list-search">
+            <div className="head-search">
+                Patient Records
+            </div>
+            <div className="list-search">
                 <input type="text" placeholder='Search'/>
             </div>
+        </div>
+
+        <div className="box-main-list">
+            <div className="head-main-list">
+                <div className="search-id">
+                    ID
+                </div>
+                <div className="search-name">
+                    Full Name
+                </div>
+                <div className="search-birth">
+                    Brithday
+                </div>
+                <div className="search-age">
+                    Age
+                </div>
+                <div className="search-gender">
+                    Gender
+                </div>
+                <div className="search-action">
+                    Action
+                </div>
+            </div>
+            <div className="main-list">
+                <div className="id-list">
+                    1234567890123
+                </div>
+                <div className="name-list">
+                    Tanawat Kama
+                </div>
+                <div className="birth-list">
+                    01/01/1111
+                </div>
+                <div className="age-list">
+                    21
+                </div>
+                <div className="gender-list">
+                    Male
+                </div>
+                <div className="action-list">
+                    Logo list
+                </div>
+            </div>
+
         </div>
     </div>
   )
