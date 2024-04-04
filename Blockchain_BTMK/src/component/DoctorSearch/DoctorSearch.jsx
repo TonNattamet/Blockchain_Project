8ad0091e5e755d56a51f5fd887245444b881df65
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-04 20:47:16 +0700

    update new search page

---
+++ Blockchain_BTMK/src/component/DoctorSearch/DoctorSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DoctorSearch() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DoctorSearch
--- Blockchain_BTMK/src/component/Search/Search.jsx
+++ Blockchain_BTMK/src/component/Search/Search.jsx
@@ -7,61 +7,21 @@ function Search() {
   return (
     <div className='main-search'>
         <Navbar/>
-        <div className="box-list-search">
-            <div className="head-search">
-                Patient Records
-            </div>
-            <div className="list-search">
-                <input type="text" placeholder='Search'/>
-            </div>
-        </div>
 
-        <div className="box-main-list-search">
-            <div className="head-main-list">
-                <div className="search-id">
-                    ID
-                </div>
-                <div className="search-name">
-                    Full Name
-                </div>
-                <div className="search-birth">
-                    Brithday
-                </div>
-                <div className="search-age">
-                    Age
-                </div>
-                <div className="search-gender">
-                    Gender
-                </div>
-                <div className="search-action">
-                    Action
-                </div>
-            </div>
-            <div className="main-list">
-                <div className="id-list">
-                    1234567890123
-                </div>
-                <div className="name-list">
-                    Tanawat Kama
-                </div>
-                <div className="birth-list">
-                    01/01/1111
-                </div>
-                <div className="age-list">
-                    21
-                </div>
-                <div className="gender-list">
-                    Male
-                </div>
-                <div className="action-list">
-                    <Link to='/profileuser'>More</Link>
-                </div>
+        <div className="box-main-search">
+
+            <h2 className='head-box-search'>Patient's ID Card Number</h2>
+
+            <div className="box-search">
+                <input type="text" placeholder='Search' />
+                <button className='button-search'>Search</button>
             </div>
 
+            <div className="profile-calcel">
+                <Link to='/homedoctor'>Back</Link>
+            </div>
         </div>
-        <div className="profile-calcel">
-            <Link to='/homedoctor'>Back</Link>
-        </div>
+        
     </div>
   )
 }
